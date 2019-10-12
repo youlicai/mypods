f26f04a7bf8bf5abe4b47fa8945c529c1baeaaec
hda <hda@hdadeiMac.local>
2019-10-12 16:10:55 +0800

    first

---
+++ Hello.h
@@ -0,0 +1,17 @@
+//
+//  Hello.h
+//  music
+//
+//  Created by hda on 2019/10/12.
+//  Copyright © 2019 Fyus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Hello : NSObject
+-(void)hello;
+@end
+
+NS_ASSUME_NONNULL_END
