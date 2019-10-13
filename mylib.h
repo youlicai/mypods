1920d9331a2724aa2b59c7a68dcc54765dd7372f
youlicai <youlicai@outlook.com>
2019-10-13 22:37:49 +0800

    add a

---
+++ mylib.h
@@ -0,0 +1,13 @@
+//
+//  mylib.h
+//  mylib
+//
+//  Created by onlyou on 2019/10/13.
+//  Copyright © 2019 onlyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mylib : NSObject
+-(void)hello;
+@end
